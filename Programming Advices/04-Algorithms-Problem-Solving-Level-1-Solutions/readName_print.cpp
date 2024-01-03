5f62d812f9d6996289a291a25e02cac75d66ec80
Benzazoua <billalben252@gmail.com>
2024-01-03 10:00:06 +0100

    Read Name And Print it

---
+++ Programming Advices/04-Algorithms-Problem-Solving-Level-1-Solutions/readName_print.cpp
@@ -0,0 +1,20 @@
+// Read Name and Print it
+#include <iostream>
+
+using namespace std;
+
+string readName() {
+  string name;
+
+  cout << "Enter Your Name: ";
+  getline(cin, name);
+  return name;
+}
+
+void printName(string name) {
+  cout << "Hello, " << name << endl;
+}
+
+int main() {
+  printName(readName());
+}
